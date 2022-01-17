6a7c9ce0998f82f774cfaa4ca71fba28c3ea9d95
Gustavo Meza <61214852+gustavo-exe@users.noreply.github.com>
2022-01-16 20:42:21 -0600

    Anidar componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AleatorioComponent } from './aleatorio/aleatorio.component';
 import { ColoresComponent } from './colores/colores.component';
+import { SaludoComponent } from './saludo/saludo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AleatorioComponent,
-    ColoresComponent
+    ColoresComponent,
+    SaludoComponent
   ],
   imports: [
     BrowserModule
