27cddb401aa7f144038d4af8b300b04593652a71
Gustavo Meza <61214852+gustavo-exe@users.noreply.github.com>
2022-01-19 19:42:57 -0600

    Enviar datos a un componente con @Input

--- src/app/saludo/saludo.component.ts
+++ src/app/saludo/saludo.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-saludo',
@@ -7,9 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SaludoComponent implements OnInit {
 
+  @Input() colorNombre:String = "";
+
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
