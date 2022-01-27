877559243f166d5697e250ff3765eead52c885d6
gustavoeliasexe <gus.elim2@gmail.com>
2022-01-27 14:52:55 +0000

    Componetes para la emulacion de paginas

--- rutas/src/app/app.module.ts
+++ rutas/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AcercaDeComponent } from './acerca-de/acerca-de.component';
+import { DetallesComponent } from './detalles/detalles.component';
+import { EncabezadoComponent } from './encabezado/encabezado.component';
+import { Error404Component } from './error404/error404.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { ListaDeLibrosComponent } from './lista-de-libros/lista-de-libros.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AcercaDeComponent,
+    DetallesComponent,
+    EncabezadoComponent,
+    Error404Component,
+    InicioComponent,
+    ListaDeLibrosComponent
   ],
   imports: [
     BrowserModule,
---
+++ rutas/src/app/lista-de-libros/lista-de-libros.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-de-libros',
+  templateUrl: './lista-de-libros.component.html',
+  styleUrls: ['./lista-de-libros.component.css']
+})
+export class ListaDeLibrosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
