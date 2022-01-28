48730f81ac137c35fc62b05980627e4c076054b6
gustavoeliasexe <gus.elim2@gmail.com>
2022-01-28 23:04:16 +0000

    Proyecto para servicios

---
+++ servicios/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AcercaDeComponent } from './acerca-de/acerca-de.component';
+import { DetallesComponent } from './detalles/detalles.component';
+import { EncabezadoComponent } from './encabezado/encabezado.component';
+import { Error404Component } from './error404/error404.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { ListaDeLibrosComponent } from './lista-de-libros/lista-de-libros.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AcercaDeComponent,
+    DetallesComponent,
+    EncabezadoComponent,
+    Error404Component,
+    InicioComponent,
+    ListaDeLibrosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
